8d87e35ea872097483e4d3bba8b25ff083d616ee
Mika Meyer <mimeyer@c1r1s4.42wolfsburg.de>
2026-05-14 17:23:55 +0200

    chore: starting api endpoints

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,5 +1,5 @@
 from loguru import logger
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Response
 
 router = APIRouter()
 
@@ -12,3 +12,23 @@ async def root():
 		"Author": "42Wolfsburg",
 		"status": "development"
 	})
+
+@router.get("/api/rooms/{room_name}/bookings", status_code=status.HTTP_202_ACCEPTED)
+async def get_room(room_name: str, response: Response):
+	return({
+		"name": room_name
+		 })
+
+@router.post("/api/rooms/{room_name}/bookings", status_code=status.HTTP_202_ACCEPTED)
+async def post_room(room_name: str, response: Response):
+	return ({
+		"result": "👍"
+    })
+
+@router.patch("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_202_ACCEPTED)
+async def patch_room(room_name: str, id: str, response: Response):
+	pass
+
+@router.delete("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_room(room_name: str, id: str, response: Response):
+	pass
