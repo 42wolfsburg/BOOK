881291b0f6b8124ef9698188c082646c657e465a
Felipe Dessoy Caraballo <felipe@42wolfsburg.de>
2026-05-15 11:51:48 +0200

    fix: pair coding for fixes w/ Mika

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -25,24 +25,24 @@ async def rooms():
 	return crud.db_get_all_bookings()
 
 #TODO id / room_name discreptancy
-@router.get("/api/rooms/{room_name}/bookings", status_code=status.HTTP_202_ACCEPTED)
-async def get_room(room_name: str, response: Response, pl: BookingRequest): 
+@router.get("/api/rooms/{room_name}/bookings", status_code=status.HTTP_200_OK)
+async def booking(room_name: str, response: Response, pl: BookingRequest): 
 	"""
 	booking data request
 
 	status: default 202, success 200, fail 400
 	"""
 	try:
-		service.get_booking()
+		resource = await service.get_booking()
 		response.status_code = status.HTTP_200_OK
+		return 
 	except Exception as err:
-		response.status_code = status.HTTP_400_BAD_REQUEST
-	finally:
+		status = status.HTTP_400_BAD_REQUEST
 		return err
 
 #TODO include payload in call
-@router.post("/api/rooms/{room_name}/bookings", status_code=status.HTTP_202_ACCEPTED)
-async def post_room(room_name: str, response: Response, pl: BookingRequest):
+@router.post("/api/rooms/{room_name}/bookings", status_code=status.HTTP_201_CREATED)
+async def booking(room_name: str, response: Response, pl: BookingRequest):
 	"""
 	booking request
 
@@ -50,18 +50,22 @@ async def post_room(room_name: str, response: Response, pl: BookingRequest):
 	"""
 	try:
 		service.register_booking(intra=pl.intra ,room_name=room_name, begin_at=str(pl.begin_at), end_at=str(pl.end_at))
-		response.status_code = status.HTTP_201_CREATED
+		# response.status_code = status.HTTP_201_CREATED
 	except ValidationError as err:
 		response.status_code = status.HTTP_400_BAD_REQUEST
+		return err
 	except Exception as err:
 		response.status_code = status.HTTP_502_BAD_GATEWAY
-	finally:
-		return err
-		
+		return err		
 
 #TODO include payload in call
-@router.patch("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_202_ACCEPTED)
-async def patch_room(room_name: str, id: str, response: Response, pl: BookingRequest):
+@router.patch("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_201_CREATED)
+async def booking(
+	room_name: str, 
+	id: str, 
+	response: Response, 
+	pl: BookingRequest
+	):
 	"""
 	booking patch
 
@@ -69,14 +73,12 @@ async def patch_room(room_name: str, id: str, response: Response, pl: BookingReq
 	"""
 	try:
 		service.update_booking(begin_at=str(pl.begin_at), end_at=str(pl.end_at) ,id=id)
-		response.status_code = status.HTTP_200_OK
 	except ValidationError as err:
 		response.status_code = status.HTTP_400_BAD_REQUEST
-	finally:
 		return err
 
 @router.delete("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_room(room_name: str, id: str, response: Response):
+async def booking(room_name: str, id: str, response: Response):
 	"""
 	booking delete
 
@@ -85,6 +87,4 @@ async def delete_room(room_name: str, id: str, response: Response):
 	try:
 		service.delete_booking(id=id)
 	except Exception as err:
-		pass
-	finally:
 		return err
