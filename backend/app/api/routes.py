d88f9df04985e603a6efd3dc6ea9e36193dc7a98
Mika Meyer <mimeyer@c2r3s7.42wolfsburg.de>
2026-05-15 14:56:27 +0200

    fix: adding dict for return value; fix: formatting function definitions; fix: changing status code to be returned instead of broadcasted

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -4,11 +4,10 @@ from pydantic import ValidationError
 
 from ..rooms.service import crud
 from ..rooms import service
-
-from .base_payload import BookingRequest
+from ..models.schemas import BookingRequest, BookingCreation, RoomName, Id
 
 router = APIRouter()
-err = ""
+response = {}
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def root():
@@ -24,67 +23,75 @@ async def root():
 async def rooms():
 	return crud.db_get_all_bookings()
 
-#TODO id / room_name discreptancy
+
 @router.get("/api/rooms/{room_name}/bookings", status_code=status.HTTP_200_OK)
-async def booking(room_name: str, response: Response, pl: BookingRequest): 
+async def booking(
+	room_name: RoomName,
+	pl: BookingRequest
+	) -> dict:
 	"""
 	booking data request
 
 	status: default 202, success 200, fail 400
 	"""
 	try:
-		resource = await service.get_booking()
-		response.status_code = status.HTTP_200_OK
+		response["resource"] = await service.get_booking()
+		response["status"] = status.HTTP_200_OK
 		return 
 	except Exception as err:
-		status = status.HTTP_400_BAD_REQUEST
-		return err
+		response["status"] = status.HTTP_400_BAD_REQUEST
+		response["error"] = err
+	return response
 
 #TODO include payload in call
 @router.post("/api/rooms/{room_name}/bookings", status_code=status.HTTP_201_CREATED)
-async def booking(room_name: str, response: Response, pl: BookingRequest):
+async def booking(
+	room_name: RoomName,
+	pl: BookingCreation
+	) -> dict:
 	"""
 	booking request
 
 	status: default 202, success 201, slot used 502, other 400
 	"""
 	try:
-		service.register_booking(intra=pl.intra ,room_name=room_name, begin_at=str(pl.begin_at), end_at=str(pl.end_at))
-		# response.status_code = status.HTTP_201_CREATED
+		["resource"] = await service.register_booking(intra=pl.intra ,room_name=room_name, begin_at=str(pl.begin_at), end_at=str(pl.end_at))
+		# response = status.HTTP_201_CREATED
 	except ValidationError as err:
-		response.status_code = status.HTTP_400_BAD_REQUEST
-		return err
+		response["status"] = status.HTTP_400_BAD_REQUEST
 	except Exception as err:
-		response.status_code = status.HTTP_502_BAD_GATEWAY
-		return err		
+		response["status"] = status.HTTP_502_BAD_GATEWAY
+		response["error"] = err
+	return response	
 
 #TODO include payload in call
 @router.patch("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_201_CREATED)
 async def booking(
-	room_name: str, 
-	id: str, 
-	response: Response, 
+	room_name: RoomName, 
+	id: Id, 
 	pl: BookingRequest
-	):
+	) -> dict:
 	"""
 	booking patch
 
 	status: default 202, success 200, fail 400
 	"""
 	try:
-		service.update_booking(begin_at=str(pl.begin_at), end_at=str(pl.end_at) ,id=id)
+		resource = await service.update_booking(begin_at=str(pl.begin_at), end_at=str(pl.end_at) ,id=id)
 	except ValidationError as err:
-		response.status_code = status.HTTP_400_BAD_REQUEST
+		response = status.HTTP_400_BAD_REQUEST
 		return err
 
 @router.delete("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def booking(room_name: str, id: str, response: Response):
+async def booking(
+	room_name: RoomName, id: Id
+	) -> dict:
 	"""
 	booking delete
 
 	status: always 204
 	"""
 	try:
-		service.delete_booking(id=id)
+		resource = await service.delete_booking(id=id)
 	except Exception as err:
 		return err
