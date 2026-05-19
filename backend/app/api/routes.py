816bc6b3ce971e0b69129f3a991cc6169f112592
MikMey <115067212+MikMey@users.noreply.github.com>
2026-05-19 10:19:32 +0200

    Api (#8)
    * feat: gitignore for root level
    
    * fix: docker compose YAML file uses proper keys for defining environment variables to the database.
    
    * feat: adding database URL for later connection to the db
    
    * fix: changing port number exposed
    
    * chore: deleting extra files
    
    * feat: created schema for backend database
    
    * fix: missing volume piece; fix: env file was not transferred to api container
    
    * chore: adding postgresql library
    
    * feat: adding lifespan to fastapi client
    
    * feat: inserting creation and closure of database connection in lifespan generator
    
    * chore: adding pool explicitly to app
    
    * feature: adding minimal database structure
    
    * chore: added endpoint for testing
    
    * chore: empty placeholder files for schemas later
    
    * feat: placeholder files
    
    * feature: SQL statements and repository functionality for backend
    
    * chore: syntax changes
    
    * feat: adding service layer to communicate with database CRUD; chore: writing documentation
    
    * fix: SQL statements correciton
    
    * feat: mock data in json format
    
    * chore: starting api endpoints
    
    * feat: adding pycache and .venv
    
    * feat: env file
    
    * feat: adding basic operations and documentation
    
    * feat: created BaseModel for payload
    
    * feat: create __init__.py for /api/
    
    * chore: adding data in payload
    
    * fix: filling service function calls; chore: adding validation for response code
    
    * fix: pair coding for fixes w/ Mika
    
    * delete: removed base_payload, moved to /backend/app/models/schema.py
    
    * fix: adding dict for return value; fix: formatting function definitions; fix: changing status code to be returned instead of broadcasted
    
    * chore: adding types for all parsed arguments of api
    
    * chore: finishing dict return; chore: formatting function calls
    
    * fix: added hyphens for compatibility with URL argument
    
    ---------
    
    Co-authored-by: Felipe Dessoy Caraballo <felipe@42wolfsburg.de>
    Co-authored-by: Mika Meyer <mimeyer@c1r1s4.42wolfsburg.de>
    Co-authored-by: Mika Meyer <mimeyer@c1r1s1.42wolfsburg.de>
    Co-authored-by: Mika Meyer <mimeyer@c2r3s7.42wolfsburg.de>

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,8 +1,13 @@
 from loguru import logger
-from fastapi import APIRouter, status
-from ..rooms.repository import crud
+from fastapi import APIRouter, status, Response
+from pydantic import ValidationError
+
+from ..rooms.service import crud
+from ..rooms import service
+from ..models.schemas import BookingRequest, BookingCreation, RoomName, Id
 
 router = APIRouter()
+response = {}
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def root():
@@ -17,3 +22,87 @@ async def root():
 @router.get("/rooms")
 async def rooms():
 	return crud.db_get_all_bookings()
+
+
+@router.get("/api/rooms/{room_name}/bookings", status_code=status.HTTP_200_OK)
+async def booking(
+	room_name: RoomName,
+	pl: BookingRequest
+	) -> dict:
+	"""
+	booking data request
+
+	status: default 202, success 200, fail 400
+	"""
+	try:
+		response["resource"] = await service.get_booking(pl.id)
+		response["status"] = status.HTTP_200_OK
+		return 
+	except Exception as err:
+		response["status"] = status.HTTP_400_BAD_REQUEST
+		response["error"] = err
+	return response
+
+#TODO include payload in call
+@router.post("/api/rooms/{room_name}/bookings", status_code=status.HTTP_201_CREATED)
+async def booking(
+	room_name: RoomName,
+	pl: BookingCreation
+	) -> dict:
+	"""
+	booking request
+
+	status: default 202, success 201, slot used 502, other 400
+	"""
+	try:
+		["resource"] = await service.register_booking(
+			intra=pl.intra,
+			room_name=room_name,
+			begin_at=str(pl.begin_at),
+			end_at=str(pl.end_at)
+			)
+		# response = status.HTTP_201_CREATED
+	except ValidationError as err:
+		response["status"] = status.HTTP_400_BAD_REQUEST
+	except Exception as err:
+		response["status"] = status.HTTP_502_BAD_GATEWAY
+		response["error"] = err
+	return response	
+
+#TODO include payload in call
+@router.patch("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_201_CREATED)
+async def booking(
+	room_name: RoomName, 
+	id: Id, 
+	pl: BookingRequest
+	) -> dict:
+	"""
+	booking patch
+
+	status: default 202, success 200, fail 400
+	"""
+	try:
+		response["resource"] = await service.update_booking(
+			begin_at=str(pl.begin_at),
+			end_at=str(pl.end_at),
+			id=id
+			)
+	except ValidationError as err:
+		response["status"] = status.HTTP_400_BAD_REQUEST
+		response["error"] = err
+	return response
+
+@router.delete("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def booking(
+	room_name: RoomName, id: Id
+	) -> dict:
+	"""
+	booking delete
+
+	status: always 204
+	"""
+	try:
+		response["resource"] = await service.delete_booking(id=id)
+	except Exception as err:
+		response["error"] = err
+	return response
