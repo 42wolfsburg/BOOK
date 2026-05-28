ce235122137f32233019d3b9d10fddcf6cebea04
Felipe Dessoy Caraballo <felipe@42wolfsburg.de>
2026-05-28 15:20:29 +0200

    feature: added scheduled cleanup that should run every three (3) hours to clear up past bookings

---
+++ backend/utils/cleanup.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from config import settings
+import psycopg2
+
+def delete_past_bookings():
+	conn=psycopg2.connect(
+		host=settings.POSTGRES_HOST,
+		database=settings.POSTGRES_DB,
+		user=settings.POSTGRES_USER,
+		password=settings.POSTGRES_PASSWORD
+	)
+	with conn.cursor as cur:
+		cur.execute("DELETE FROM bookings WHERE end_time < %s", (datetime.now(),))
+	conn.commit()
+
