7737b33b2d5c311fc0897e89157d8b6497eccbd4
Felipe Dessoy Caraballo <felipe@42wolfsburg.de>
2026-06-01 12:42:02 +0200

    chore: adding environment variables

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,3 @@
-# .env          → stores the raw values (passwords, credentials)
-# config.py     → reads .env and makes them available in Python
-# database.py   → imports config.py to get database credentials
-# Stop using AI to generate your stuff
-
 from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
 
@@ -15,10 +10,13 @@ class Settings(BaseSettings):
 	BACKEND_HOST: str
 	BACKEND_PORT: int
 	DATABASE_URL: str
+	JWT_SECRET: str
+	REDIRECT_URI: str
+	CLIENT_ID: str
+	SECRET: str
+
 	VITE_API_URL: str
-	VITE_42_SECRET: str
-	VITE_42_CLIENT_ID: str
-	VITE_42_REDIRECT_URI: str
+
 	model_config = ConfigDict(env_file=".env")
 
 settings = Settings()
