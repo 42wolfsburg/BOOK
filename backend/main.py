cc64c8924d73e215367f731459dd0b7353f55180
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-06-25 16:27:41 +0200

    fix: improper import name now has proper import name

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, CORSMiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 from contextlib import asynccontextmanager
 from app.api.routes import router
