69701b9326db3e68a5f9a8c0e666b4178d5f14bd
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-07-03 13:43:49 +0200

    feature: minimal structure for bookings services API bridging

---
+++ frontend/src/services/bookingService.js
@@ -0,0 +1,79 @@
+import { apiRequest } from './Api'
+
+export const getAllBookings = async () => {
+    let response;
+    try {
+        response = await apiRequest("/api/rooms", {
+            method: "GET",
+        });
+    } catch (e) {
+        throw e;
+    }
+    return response
+}
+
+export const getBookingById = async ({room_name, id}) => {
+    let response;
+    
+    try {
+        response = await apiRequest(`/api/rooms/${room_name}/bookings/${id}`, {
+            method: "GET",
+        })
+    } catch (e) {
+        throw e;
+    }
+    return response
+}
+
+export const getBookings = async (room_name) => {
+    let response;
+    try {
+        response = await apiRequest(`/api/rooms/${room_name}/bookings`, {
+            method: "GET",
+        });
+    } catch (e) {
+        throw e;
+    }
+    return response
+}
+
+export const postBookings = async (room_name) => {
+    let response;
+    
+    try {
+        response = await apiRequest(`/api/rooms/${room_name}/bookings`, {
+            method: "POST",
+            body: '',
+        });
+    } catch (e) {
+        throw e;
+    }
+    return response
+}
+
+export const patchBookingById = async ({room_name, id}) => {
+    let response;
+    try {
+        response = await apiRequest(`/api/rooms/${room_name}/bookings/${id}`, {
+            method: "PATCH",
+            body: '',
+        });
+    } catch (e) {
+        throw e;
+    }
+    return response
+}
+
+export const deleteBookingById = async ({room_name, id}) => {
+    let response;
+    
+    try {
+        response = await apiRequest(`/api/rooms/${room_name}/bookings/${id}`, {
+            method: "DELETE"
+        });
+    } catch (e) {
+        throw e;
+    }
+    return response
+}
+
