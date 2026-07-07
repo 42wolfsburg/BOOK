d6acec0e23bf25d25cbfefca398e5837c86ce4f7
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-07-07 10:27:44 +0200

    feature: changing hook to plug with services and backend

--- frontend/src/hooks/useBookings.js
+++ frontend/src/hooks/useBookings.js
@@ -1,36 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../components/AuthGate'
+import { getBookings, postBookings } from '../services/bookingService'
 
-export default function useBookings(selectedRoom, eventsData) {
-  const [events, setEvents] = useState(eventsData[selectedRoom] || []);
+export default function useBookings(currentRoom) {
+  const [events, setEvents] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [bookingData, setBookingData] = useState(null);
+  const login = useContext(AuthContext)
 
   useEffect(() => {
-    setEvents(eventsData[selectedRoom] || []);
-  }, [selectedRoom, eventsData]);
+    async function loadBookings() {
+      const bookings = await getBookings(currentRoom.slug);
+      setEvents(
+        bookings.map((b) => ({
+          title: `Booked by ${b.intra}`,
+          start: new Date(b.begin_at),
+          end: new Date(b.end_at),
+        }))
+      );
+    }
+    loadBookings();
+  }, [currentRoom]);
 
   const openBookingModal = (slot) => {
-    setBookingData({
-      start: slot.start,
-      end: slot.end,
-    });
+    setBookingData({ start: slot.start, end: slot.end });
     setShowModal(true);
   };
 
-  const saveBooking = () => {
+  const saveBooking = async () => {
     if (!bookingData) return;
-    setEvents((prev) => [...prev, bookingData]);
+
+    const payload = {
+      room_name: currentRoom.slug,
+      intra: login,
+      begin_at: Math.floor(bookingData.start.getTime() / 1000),
+      end_at: Math.floor(bookingData.end.getTime() / 1000),
+    };
+
+    const response = await postBookings(payload)
+
+    setEvents((prev) => [
+      ...prev, 
+      { title: `Booked by ${login}`, start: bookingData.start, end: bookingData.end }
+    ]);
     setShowModal(false);
   };
 
   return {
-    events,
-    setEvents,
-    showModal,
-    setShowModal,
-    bookingData,
-    setBookingData,
-    openBookingModal,
-    saveBooking,
+    events, setEvents, showModal, setShowModal,
+    bookingData, setBookingData, openBookingModal, saveBooking,
   };
 }
