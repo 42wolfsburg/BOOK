bcd76981bd0c83ac0f0e70d12a9c0e013112d86f
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2026-07-07 10:28:27 +0200

    fix: changing data to match backend

--- frontend/src/data/rooms.js
+++ frontend/src/data/rooms.js
@@ -1,39 +1,46 @@
+// export const rooms = [
+//   {
+//     id: 1,
+//     name: "Piscine meeting room",
+//     //seats: 10,
+//     color:
+//       "bg-blue-50 border-blue-200 text-blue-600",
+//     accent: "#3B82F6",
+//   },
+
+//   {
+//     id: 2,
+//     name: "Galaxy meeting room",
+//     //seats: 8,
+//     color:
+//       "bg-emerald-50 border-emerald-200 text-emerald-600",
+//     accent: "#10B981",
+//   },
+
+//   {
+//     id: 3,
+//     name: "Space invaders meeting room",
+//     //seats: 12,
+//     color:
+//       "bg-violet-50 border-violet-200 text-violet-600",
+//     accent: "#8B5CF6",
+//   },
+
+//   {
+//     id: 4,
+//     name: "Gallery meeting room",
+//     //seats: 4,
+//     color:
+//       "bg-orange-50 border-orange-200 text-orange-500",
+//     accent: "#F97316",
+//   },
+// ];
+
 export const rooms = [
-  {
-    id: 1,
-    name: "Piscine meeting room",
-    //seats: 10,
-    color:
-      "bg-blue-50 border-blue-200 text-blue-600",
-    accent: "#3B82F6",
-  },
-
-  {
-    id: 2,
-    name: "Galaxy meeting room",
-    //seats: 8,
-    color:
-      "bg-emerald-50 border-emerald-200 text-emerald-600",
-    accent: "#10B981",
-  },
-
-  {
-    id: 3,
-    name: "Space invaders meeting room",
-    //seats: 12,
-    color:
-      "bg-violet-50 border-violet-200 text-violet-600",
-    accent: "#8B5CF6",
-  },
-
-  {
-    id: 4,
-    name: "Gallery meeting room",
-    //seats: 4,
-    color:
-      "bg-orange-50 border-orange-200 text-orange-500",
-    accent: "#F97316",
-  },
+  { id: 1, name: "Piscine meeting room", slug: "piscine"},
+  { id: 2, name: "Galaxy meeting room", slug: "galaxy"},
+  { id: 3, name: "Space invaders meeting room", slug: "space-invader"},
+  { id: 4, name: "Gallery meeting room", slug: "gallery"},
 ];
 
 export const eventsData = {
