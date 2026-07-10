4c2be6c0db6b13bfd8612038ba0575a1d14d58ed
root <root@ubuntu.42campus.org>
2026-07-10 11:51:19 +0200

    fix: breaking down routes to respective concerns

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,25 +1,15 @@
-import secrets
-import httpx
-import jwt
 from uuid import UUID
 from typing import Annotated
 from urllib.parse import urlencode
 from loguru import logger
 from fastapi import APIRouter, status, HTTPException, Request, Response, Cookie, Depends
-from fastapi.responses import RedirectResponse
-from datetime import datetime, timedelta, timezone
 
 from ..rooms import service
 from ..auth.auth import get_current_user
 from config import settings
 from ..models.schemas import BookingRequest, BookingCreation, RoomName
 
-router = APIRouter()
-response = {}
-
-FT_AUTH_URL  = "https://api.intra.42.fr/oauth/authorize"
-FT_TOKEN_URL = "https://api.intra.42.fr/oauth/token"
-FT_USER_URL  = "https://api.intra.42.fr/v2/me"
+api_router = APIRouter()
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def root():
@@ -31,123 +21,12 @@ async def root():
 		"status": "development"
 	})
 
-@router.get("/auth/me", status_code=200) #check already authenticated user
-async def me(user: Annotated[dict, Depends(get_current_user)]):
-	return {
-		"login": user["login"],
-		"is_staff": user["is_staff"]
-	}
-	# try:
-	# 	payload = jwt.decode(session, key=settings.JWT_SECRET, algorithms=["HS256"])
-	# 	return {
-	# 		"login": payload["login"],
-	# 		"is_staff": payload["is_staff"]
-	# 	  }
-	# except jwt.ExpiredSignatureError as e:
-	# 	logger.error(f'ERROR: Expired signature in token {e}')
-	# 	raise HTTPException(status_code=401, detail=str(e))
-	# except jwt.InvalidTokenError as e:
-	# 	logger.error(f'ERROR: Invalid token {e}')
-	# 	raise HTTPException(status_code=401, detail=str(e))
 
-@router.get("/auth/login", status_code=302) #redirect HTTP code
-async def login():
-	state = secrets.token_urlsafe(32)
-
-	auth_url = FT_AUTH_URL + "?" + urlencode({
-		"client_id":		settings.CLIENT_ID,
-		"redirect_uri":		settings.REDIRECT_URI,
-		"response_type":	"code",
-		"scope":			"public",
-		"state":			state
-	})
-
-	response = RedirectResponse(url=auth_url, status_code=302)
-	response.set_cookie(
-		key="oauth_state",
-		value=state,
-		httponly=True,
-		secure=False, # change in prod
-		samesite="lax",
-		max_age=500
-	)
-
-	return response
-
-@router.get("/auth/logout", status_code=200)
-async def logout(response: Response):
-	response.delete_cookie(
-		key="session",
-		httponly=True,
-		samesite="lax"
-	)
-	return {"message": "logged out"}
-
-@router.get("/auth/callback", status_code=302) #redirect HTTP code
-async def callback(request: Request, code: str, state: str):
-	stored_state = request.cookies.get("oauth_state")
-	if not stored_state or stored_state != state:
-		raise HTTPException(status_code=400, detail="Invalid code")
-	
-	try:
-		async with httpx.AsyncClient(timeout=httpx.Timeout(30.0, connect=30.0)) as client:
-			token_res = await client.post(FT_TOKEN_URL, data={
-				"grant_type":		"authorization_code",
-				"client_id":		settings.CLIENT_ID,
-				"client_secret":	settings.SECRET,
-				"code":				code,
-				"redirect_uri":		settings.REDIRECT_URI,
-			})
-
-		if token_res.status_code != 200:
-			raise HTTPException(status_code=400, detail="Failed to exchange code")
-
-		ft_access_token = token_res.json()["access_token"]
-
-		async with httpx.AsyncClient(timeout=httpx.Timeout(30.0, connect=30.0)) as client:
-			user_res = await client.get(
-				FT_USER_URL,
-				headers={"Authorization": f"Bearer {ft_access_token}"}
-			)
-
-		if user_res.status_code != 200:
-			raise HTTPException(status_code=400, detail="Failed to fetch user")
-
-		user = user_res.json()
-
-		session_token = jwt.encode(
-			payload={
-				"sub":		str(user["id"]),
-				"login":	user["login"],
-				"is_staff":	user["staff?"],
-				"exp":		datetime.now(timezone.utc) + timedelta(days=7)
-			},
-			key=settings.JWT_SECRET,
-			algorithm="HS256"
-		)
-		response = RedirectResponse(url=settings.FRONTEND_URL, status_code=302)
-		response.set_cookie(
-			key="session",
-			value=session_token,
-			httponly=True,
-			secure=False, # change in prod
-			samesite="lax",
-			max_age=60*60*24*7
-		)
-		response.delete_cookie("oauth_state")
-		return response
-	except httpx.TimeoutException:
-		logger.error("Timeout during token exchange with 42")
-		return RedirectResponse(
-			url=f"{settings.FRONTEND_URL}/login?error=timeout",
-			status_code=302
-		)
-
-# @router.get("/api/rooms")
+# @router.get("/rooms")
 # async def rooms():
 # 	return await service.get_all_bookings()
 
-@router.get("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_200_OK)
+@router.get("/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_200_OK)
 async def booking(
 	room_name: RoomName,
 	id: UUID
@@ -166,7 +45,7 @@ async def booking(
 	except Exception as err:
 		raise HTTPException(status_code=404, detail=str(err))
 
-@router.get("/api/rooms/{room_name}/bookings", status_code=status.HTTP_200_OK)
+@router.get("/rooms/{room_name}/bookings", status_code=status.HTTP_200_OK)
 async def booking(room_name: RoomName) -> dict:
 	"""
 	booking data request
@@ -181,7 +60,7 @@ async def booking(room_name: RoomName) -> dict:
 	except Exception as err:
 		raise HTTPException(status_code=404, detail=str(err))
 
-@router.post("/api/rooms/{room_name}/bookings", status_code=status.HTTP_201_CREATED)
+@router.post("/rooms/{room_name}/bookings", status_code=status.HTTP_201_CREATED)
 async def booking(
 	room_name: RoomName,
 	pl: BookingCreation,
@@ -204,7 +83,7 @@ async def booking(
 	except Exception as e:
 		raise HTTPException(status_code=400, detail=str(e))
 
-@router.patch("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_201_CREATED)
+@router.patch("/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_201_CREATED)
 async def booking(
 	room_name: RoomName, 
 	pl: BookingRequest,
@@ -226,7 +105,7 @@ async def booking(
 	except Exception as err:
 		raise HTTPException(status_code=400, detail=str(err))
 
-@router.delete("/api/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/rooms/{room_name}/bookings/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def booking(
 	room_name: RoomName,
 	id: UUID,
